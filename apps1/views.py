cc4716277e06807dcafd7ca604186ec12ffa0589
Md Rakib Hassan <rakib1515hassan@gmail.com>
2025-02-17 22:56:11 +0600

    update

--- apps1/views.py
+++ apps1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
 def home(request):
@@ -8,11 +11,18 @@ def home(request):
     dep  = "Information Technology"
     hobby = ['Playing Cricket', 'Programming', 'Reading Book']
 
+    parents = {
+        'father_name': 'Md Shahjahan Mia',
+        'mother_name': "Rashida Bagum",
+        'city': 'Chandpure',
+    }
+
     context = {
         'name': name,
         'designation': des,
         'department' : dep,
         'hobby' : hobby,
+        'parents' : parents,
     }
 
     return render(request, 'index.html', context)
